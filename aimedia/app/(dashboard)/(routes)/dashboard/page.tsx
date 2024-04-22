a49ec9fdc8c1054f6af8a74aa972a3ac8d97e0f0
pratyush021 <probohidar2016@gmail.com>
2024-04-22 15:53:03 +0530

    added dashboard and landing page template

---
+++ aimedia/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,7 @@
+const DashboardPage=()=> {
+    return <div>
+        dashboard page... (Unprotected)
+    </div>
+}
+
+export default DashboardPage; 
---
+++ aimedia/app/(landing)/page.tsx
@@ -0,0 +1,7 @@
+const Page=()=> {
+    return <div>
+        Landing page... (Unprotected)
+    </div>
+}
+
+export default Page; 
